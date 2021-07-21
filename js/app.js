660ccc2782f843228cc763067a85345742633e4b
Leonel Rojas <levisrojas@hotmail.es>
2021-07-21 15:31:42 -0300

    presupuestoV2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 const ingresos = [
     new Ingreso("Salario",2100),
     new Ingreso("Venta Coche", 1500),
-    new Ingreso("Venta IPAD", 400)
+    
 ]
 const egresos = [
     new Egreso("Pago Renta", 1000),
     new Egreso("Ropa",500),
-    new Egreso("Ipad pro", 999)
+    
 ]
 
 let cargarApp = ()=> {
